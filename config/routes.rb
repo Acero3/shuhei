4f8a4c043041c92fdd2e78a38b8093ce1a56befe
Acero3 <app1ep1e55T@gmail.com>
2024-02-13 23:19:56 +0900

    add second

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "top#index"
-  get "/", to: "second#index"
+  get "/second", to: "second#index"
 end
